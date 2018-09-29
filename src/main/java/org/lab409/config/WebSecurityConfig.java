6cb70335706c775fd61da31be987405000a51e7a
chloeyin <chloeyinbetter@outlook.com>
2018-09-29 08:43:33 +0800

    add register router

--- src/main/java/org/lab409/config/WebSecurityConfig.java
+++ src/main/java/org/lab409/config/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
             // Un-secure H2 Database
             .antMatchers("/h2-console/**/**").permitAll()
-
+            .antMatchers("/register/**").permitAll()
             .antMatchers("/auth/**").permitAll()
             .anyRequest().authenticated();
 
