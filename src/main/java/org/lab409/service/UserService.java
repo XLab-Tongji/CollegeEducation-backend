f6b74ab5eb96dfbc95192d935f3ee96495742f45
chloeyin <chloeyinbetter@outlook.com>
2018-09-29 08:48:20 +0800

    add user service

--- src/main/java/org/lab409/service/UserService.java
+++ src/main/java/org/lab409/service/UserService.java
@@ -3,5 +3,5 @@ package org.lab409.service;
 import org.lab409.entity.*;
 
 public interface UserService {
-    public boolean insertUser(UserEntity user);
+    boolean insertUser(UserEntity user);
 }
