5cb74dc70ed86aebae42cc1599a0671c9ac1c82c
chloeyin <chloeyinbetter@outlook.com>
2018-10-11 18:37:18 +0800

    add delete, download api(simple version)

--- src/main/java/org/lab409/controller/ResourceController.java
+++ src/main/java/org/lab409/controller/ResourceController.java
@@ -4,19 +4,23 @@ import javafx.util.Pair;
 import org.lab409.entity.ResponseMessage;
 import org.lab409.service.ResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class ResourceController {
     @Autowired
     private ResourceService resourceService;
 
-    @RequestMapping(path = "/upload", method = RequestMethod.POST)
-    public ResponseMessage uploadResourceController(@RequestParam(name = "file") MultipartFile uploadResource,
+    @RequestMapping(path = "/uploadResource", method = RequestMethod.POST)
+    public ResponseMessage uploadResourceController(@RequestParam(name = "resource") MultipartFile uploadResource,
                                                     @RequestParam(name = "resourceName") String resourceName,
                                                     @RequestParam(name = "description") String description ) {
         Pair<Boolean, String> success = resourceService.uploadResource(uploadResource, resourceName, description);
@@ -25,6 +29,44 @@ public class ResourceController {
         }
         else {
             return new ResponseMessage<>(success.getValue()).error(202, "upload fail");
+        }
+    }
+
+    @RequestMapping(path = "/downloadResource/{resourceID}", method = RequestMethod.GET)
+    public ResponseEntity downloadResourceController(@PathVariable("resourceID") String resourceID) {
+        Pair<Boolean, GridFsResource> success = resourceService.downloadResource(resourceID);
+        if (success.getKey()) {
+            GridFsResource gridFsResource = success.getValue();
+            try {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentDispositionFormData("attachment", gridFsResource.getFilename());
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(gridFsResource.getContentType()))
+                        .contentLength(gridFsResource.contentLength())
+                        .headers(headers)
+                        .body(gridFsResource);
+
+            }
+            catch (IOException e) {
+                return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        else {
+            return new ResponseEntity((HttpStatus.NOT_FOUND));
+        }
+    }
+
+    @RequestMapping(path = "/deleteResource/{resourceID}", method = RequestMethod.DELETE)
+    public ResponseMessage deleteResourceController(@PathVariable("resourceID") String resourceID) {
+        int success = resourceService.deleteResource(resourceID);
+        if (success == -1) {
+            return new ResponseMessage<>(null).error(202, "file not found");
+        }
+        else if (success == -2) {
+            return new ResponseMessage<>(null).error(401, "unauthorized deletion");
+        }
+        else {
+            return new ResponseMessage<>(null).success();
         }
     }
 }
