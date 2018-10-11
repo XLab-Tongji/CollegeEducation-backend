87c815a3f9b3954613514b3c3c4ab17719392c60
chloeyin <chloeyinbetter@outlook.com>
2018-10-11 20:11:11 +0800

    finish delete & download api

--- src/main/java/org/lab409/service/ResourceService.java
+++ src/main/java/org/lab409/service/ResourceService.java
@@ -1,10 +1,11 @@
 package org.lab409.service;
 
+import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.web.multipart.MultipartFile;
 import javafx.util.Pair;
 
 public interface ResourceService {
     Pair<Boolean, String> uploadResource(MultipartFile resource, String resourceName, String description);
-    //boolean downloadResource();
-    //boolean deleteResource();
+    Pair<Boolean, GridFsResource> downloadResource(String resourceID);
+    int deleteResource(String resourceID);
 }
--- src/main/java/org/lab409/service/impl/ResourceServiceImpl.java
+++ src/main/java/org/lab409/service/impl/ResourceServiceImpl.java
@@ -1,11 +1,15 @@
 package org.lab409.service.impl;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import com.oracle.tools.packager.Log;
 import org.lab409.entity.UserEntity;
 import org.lab409.service.ResourceService;
 import org.lab409.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -54,8 +58,38 @@ public class ResourceServiceImpl implements ResourceService {
         return new Pair<>(false, "");
     }
 
-    /*@Override
-    public boolean downloadResource() {
+    @Override
+    public Pair<Boolean, GridFsResource> downloadResource(String resourceID) {
+        GridFSFile gridFSFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(resourceID)));
+        if (gridFSFile == null) {
+            return new Pair<>(false, null);
+        }
+        String gridFileName = gridFSFile.getFilename();
+        GridFsResource[] gridFsResources = gridFsTemplate.getResources(gridFileName);
+        for(GridFsResource gridFsResource : gridFsResources ) {
+            if (gridFsResource.getId().toString().equals(gridFSFile.getId().toString())){
+                return new Pair<>(true, gridFsResource);
+            }
+        }
+        return new Pair<>(false, null);
+    }
 
-    }*/
+    @Override
+    public int deleteResource(String resourceID) {
+        /* -1: can't find file
+           -2: no authority
+           0: ok
+        */
+        UserEntity userEntity = userUtil.getCurrentUser();
+        Query query = new Query(Criteria.where("_id").is(resourceID));
+        GridFSFile gridFSFile = gridFsTemplate.findOne(query);
+        if (gridFSFile == null) {
+            return -1;
+        }
+        if (gridFSFile.getMetadata().get("uploader") != userEntity.getUserID() ) {
+            return -2;
+        }
+        gridFsTemplate.delete(query);
+        return 0;
+    }
 }
