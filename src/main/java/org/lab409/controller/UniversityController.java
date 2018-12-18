d19afc880d8bc00fd110d276ef8a39e0f4e1eecf
chloeyin <chloeyinbetter@outlook.com>
2018-12-19 00:56:02 +0800

    sb

--- src/main/java/org/lab409/controller/UniversityController.java
+++ src/main/java/org/lab409/controller/UniversityController.java
@@ -18,14 +18,14 @@ public class UniversityController {
     @Autowired
     private UniversityService universityService;
 
-    @RequestMapping(path = "register/university", method = RequestMethod.GET)
+    @RequestMapping(path = "/register/university", method = RequestMethod.GET)
     public ResponseMessage universityController() {
 
         List<UniversityEntity> universities = universityService.getUniversities();
         return new ResponseMessage<>(universities).success();
     }
 
-    @RequestMapping(path = "register/university/{universityID}",method = RequestMethod.GET)
+    @RequestMapping(path = "/register/university/{universityID}",method = RequestMethod.GET)
     public ResponseMessage majorByUniversityController(@PathVariable("universityID") Integer universityID) {
         List<MajorEntity> majors = universityService.getMajorByUniversity(universityID);
         return new ResponseMessage<>(majors).success();
